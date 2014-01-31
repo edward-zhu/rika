2305acf638f3b1d94a8d9e3ee8b94fc3407c2502
祝嘉栋 <EdwardZhu@shuku-MacBook-Pro.local>
2014-02-01 07:25:26 +0800

    accelerate loading speed.

--- public/javascript/survey.js
+++ public/javascript/survey.js
@@ -1,6 +1,20 @@
 var data;
 var questions = [];
 var taker_token;
+var html_sing, html_multi, html_text;
+
+var rules = 
+	{
+		checkbox : {
+			identifier : 'answer',
+			rules : [
+				{
+					type: "checked_check",
+					prompt : "请至少选择一个答案。"
+				}
+			]
+		}
+	};
 
 function sortBy(a, b) {
 	return a.id - b.id;
@@ -29,155 +43,118 @@ function getPos(id, answers) {
 function loadSingleQuestion(question, total, callback) {
 	var data = question;
 	data.total = total;
-	
-	
-	$.get('/partials/question_single.html',{}, function (html) {
-		$("#question_form").html(Mustache.render(html, data));
-		callback();
-		$(".ui.checkbox").checkbox();
-		$(".ui.form").form({
-			checkbox : {
-				identifier : 'answer',
-				rules : [
-					{
-						type: "checked_check",
-						prompt : "请至少选择一个答案。"
-					}
-				]
+	$("#question_form").html(Mustache.render(html_sing, data));
+	callback();
+	$(".ui.checkbox").checkbox();
+	$(".ui.form").form(rules,{
+		onSuccess: function () {
+			$("#question_form").addClass("loading");
+			var answer_id = $("input:checked").val();
+			var answers = [
+				{
+					answer_id : answer_id
+				}
+			]
+			var pos = getPos(answer_id, question.answers);
+			var next = question.id + 1;
+			if (question.next) {
+				next = question.next;
 			}
-		},{
-			onSuccess: function () {
-				$("#question_form").addClass("loading");
-				var answer_id = $("input:checked").val();
-				var answers = [
-					{
-						answer_id : answer_id
-					}
-				]
-				var pos = getPos(answer_id, question.answers);
-				var next = question.id + 1;
-				if (question.next) {
-					next = question.next;
-				}
-				if (question.answers[pos].next) {
-					next = question.answers[pos].next;
-				}
-				$.ajax('/response',{
-					data : {
-						taker_token	: taker_token,
-						survey_id 	: $("#survey_id").val(),
-						answers		: answers,
-						question_id	: question._id
-					},
-					type : 'POST'
-				}).done(function (data) {
-					console.log(next);
-					
-					loadQuestion(next);
-				});
-			},
-			rules : {
-				checked_check : function () {
-					return $("input:checked").length > 0;
-				}
+			if (question.answers[pos].next) {
+				next = question.answers[pos].next;
 			}
-		});
-		
+			$.ajax('/response',{
+				data : {
+					taker_token	: taker_token,
+					survey_id 	: $("#survey_id").val(),
+					answers		: answers,
+					question_id	: question._id
+				},
+				type : 'POST'
+			}).done(function (data) {
+				console.log(next);
+				
+				loadQuestion(next);
+			});
+		},
+		rules : {
+			checked_check : function () {
+				return $("input:checked").length > 0;
+			}
+		}
 	});
 }
 
 function loadMultipleQuestion(question, total, callback) {
 	var data = question;
 	data.total = total;
-	$.get('/partials/question_multiple.html',{}, function (html) {
-		$("#question_form").html(Mustache.render(html, data));
-		callback();
-		$(".ui.checkbox").checkbox();
-		$(".ui.form").form({
-			checkbox : {
-				identifier : 'answer',
-				rules : [
-					{
-						type: "checked_check",
-						prompt : "请至少选择一个答案。"
-					}
-				]
-			}
-		},{
-			onSuccess: function () {
-				$("#question_form").addClass("loading");
-				var answers = [];
-				$("input:checked").each(function (i, answer) {
-					answers[i] = {
-						answer_id : $(answer).val()
-					}
-				});
-				console.log(answers);
-				var next = question.id + 1;
-				if (question.next) {
-					next = question.next;
+	$("#question_form").html(Mustache.render(html_multi, data));
+	callback();
+	$(".ui.checkbox").checkbox();
+	$(".ui.form").form(rules ,{
+		onSuccess: function () {
+			$("#question_form").addClass("loading");
+			var answers = [];
+			$("input:checked").each(function (i, answer) {
+				answers[i] = {
+					answer_id : $(answer).val()
 				}
-				$.ajax('/response',{
-					data : {
-						taker_token	: taker_token,
-						survey_id 	: $("#survey_id").val(),
-						answers		: answers,
-						question_id	: question._id
-					},
-					type : 'POST'
-				}).done(function (data) {
-					// console.log(data);
-					
-					loadQuestion(next);
-				});
-			},
-			rules : {
-				checked_check : function () {
-					return $("input:checked").length > 0;
-				}
+			});
+			console.log(answers);
+			var next = question.id + 1;
+			if (question.next) {
+				next = question.next;
 			}
-		});
-		
+			$.ajax('/response',{
+				data : {
+					taker_token	: taker_token,
+					survey_id 	: $("#survey_id").val(),
+					answers		: answers,
+					question_id	: question._id
+				},
+				type : 'POST'
+			}).done(function (data) {
+				loadQuestion(next);
+			});
+		},
+		rules : {
+			checked_check : function () {
+				return $("input:checked").length > 0;
+			}
+		}
 	});
 }
 
-function loadTextQuestion(question, total, callback)
-{
+function loadTextQuestion(question, total, callback) {
 	var data = question;
 	data.total = total;
-	$.get('/partials/question_text.html',{}, function (html) {
-		$("#question_form").html(Mustache.render(html, data));
-		callback();
-		$(".ui.form").form({
-
-		},{
-			onSuccess: function () {
-				$("#question_form").addClass("loading");
-				var answers = [{
-					answer_id	: question._id,
-					answer		: $("#answer").val()
-				}];
-				console.log(answers);
-				var next = question.id + 1;
-				if (question.next) {
-					next = question.next;
-				}
-				$.ajax('/response',{
-					data : {
-						taker_token	: taker_token,
-						survey_id 	: $("#survey_id").val(),
-						answers		: answers,
-						question_id	: question._id
-					},
-					type : 'POST'
-				}).done(function (data) {
-					// console.log(data);
-					
-					loadQuestion(next);
-				});
+	
+	$("#question_form").html(Mustache.render(html_text, data));
+	callback();
+	$(".ui.form").form({},{
+		onSuccess: function () {
+			$("#question_form").addClass("loading");
+			var answers = [{
+				answer_id	: question._id,
+				answer		: $("#answer").val()
+			}];
+			console.log(answers);
+			var next = question.id + 1;
+			if (question.next) {
+				next = question.next;
 			}
-		});
-		
+			$.ajax('/response',{
+				data : {
+					taker_token	: taker_token,
+					survey_id 	: $("#survey_id").val(),
+					answers		: answers,
+					question_id	: question._id
+				},
+				type : 'POST'
+			}).done(function (data) {
+				loadQuestion(next);
+			});
+		}
 	});
 }
 
@@ -191,7 +168,6 @@ function loadQuestion(id) {
 	var question = questions[id - 1];
 	var total = questions.length;
 	if (id > total) {
-		
 		finish();
 		return;
 	}
@@ -211,19 +187,36 @@ function loadQuestion(id) {
 	}
 }
 
+function loadUI(callback) {
+	$.get('/partials/question_single.html', function (html) {
+		html_sing = html;
+		$.get('/partials/question_multiple.html', function (html) {
+			html_multi = html;
+			$.get('/partials/question_text.html', function (html) {
+				html_text = html;
+				callback();
+			});
+		});
+	});
+}
 
 $(document).ready(function () {
 	$("#go").addClass("disabled");
-	loadQuestions(function (data) {
-		questions = data;
-		// console.log(questions);
-		$("#go").removeClass("disabled");
-		$("#go").html("<i class='ui play icon'></i>开始");
+	$("#go").html("<i class='ui play icon'></i>加载UI..");
+	loadUI(function () {
+		$("#go").html("<i class='ui play icon'></i>加载调查内容..");
+		loadQuestions(function (data) {
+			questions = data;
+			$("#go").removeClass("disabled");
+			$("#go").html("<i class='ui play icon'></i>开始");
+		});
+		taker_token = CryptoJS.SHA1(Date() + $("#survey_id").val() + Math.random().toString()).toString();
 	});
-	taker_token = CryptoJS.SHA1(Date() + $("#survey_id").val() + Math.random().toString()).toString();
+	
 });
 
 
 $("#go").click(function () {
+	$("#question_form").addClass("loading");
 	loadQuestion(1);	
 })
